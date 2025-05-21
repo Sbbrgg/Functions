8589832470e8259f480ef33c7c8694b4c5ef3b96
xromo <xromo@DESKTOP-J6CQG2O>
2025-05-21 16:39:13 +0300

    Homework Prt.1

---
+++ Arrays/main.cpp
@@ -0,0 +1,166 @@
+#include <iostream>
+using namespace std;
+
+#define tab "\t"
+
+void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
+void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);
+
+void Print(int arr[], const int n);
+void Print(double arr[], const int n);
+
+void Sort(int arr[], const int n);
+void Sort(double arr[], const int n);
+
+int Sum(int arr[], const int n);
+double Avg(int arr[], const int n);
+int MinValueIn(int arr[], const int n);
+int MaxValueIn(int arr[], const int n);
+void ShiftLeft(int arr[], const int n, int shift);
+void ShiftRight(int arr[], const int n, int shift);
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int n = 5;
+	int arr[n], shift;
+	FillRand(arr, n);
+	Print(arr, n);
+	cout << "Сумма элементов массива = " << Sum(arr, n) << endl;
+	cout << "Среднее арифметическое элементов массива = " << Avg(arr, n) << endl;
+	cout << "Минимальный элемент массива: " << MinValueIn(arr, n) << endl;
+	cout << "Максимальный элемент массива: " << MaxValueIn(arr, n) << endl;
+	cout << "Введите число, на которое нужно сделать сдвиг массива влево: "; cin >> shift;
+	cout << "Сдвинутый массив влево: ";
+	ShiftLeft(arr, n, shift);
+	Print(arr, n);
+	cout << "Введите число, на которое нужно сделать сдвиг массива вправо: "; cin >> shift;
+	cout << "Сдвинутый массив вправо: ";
+	ShiftRight(arr, n, shift);
+	Print(arr, n);
+
+}
+void FillRand(int arr[], const int n, int minRand, int maxRand)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+	}
+}
+void FillRand(double arr[], const int n, int minRand, int maxRand)
+{
+	minRand *= 100;
+	maxRand *= 100;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+		arr[i] /= 100;
+	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+void Print(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+void Sort(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+void Sort(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				double buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+
+int Sum(int arr[], const int n)
+{
+	int rez = 0;
+	for (int i = 0; i < n; i++)
+	{
+		rez += arr[i];
+	}
+	return rez;
+}
+double Avg(int arr[], const int n)
+{
+	double rez = 0;
+	for (int i = 0; i < n; i++)
+	{
+		rez = rez + arr[i];
+	}
+	return double(rez / n);
+}
+int MinValueIn(int arr[], const int n)
+{
+	int minElement = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < minElement) minElement = arr[i];
+	}
+	return minElement;
+}
+int MaxValueIn(int arr[], const int n)
+{
+	int maxElement = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > maxElement) maxElement = arr[i];
+	}
+	return maxElement;
+}
+void ShiftLeft(int arr[], const int n, int shift)
+{
+	for (int i = 0; i < shift; i++)
+	{
+		int buffer = arr[0];
+		for (int i = 1; i < n; i++)
+		{
+			arr[i - 1] = arr[i];
+		}
+		arr[n - 1] = buffer;
+	}
+}
+void ShiftRight(int arr[], const int n, int shift)
+{
+	for (int i = 0; i < shift; i++)
+	{
+		int buffer = arr[n - 1];
+		for (int j = n - 1; j > 0; j--)
+		{
+			arr[j] = arr[j - 1];
+		}
+		arr[0] = buffer;
+	}
+}
